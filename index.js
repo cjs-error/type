c652ca510435b98c46cba82b60bd6091fcceb172
Rodney Teal <rodneyd.teal@gmail.com>
2015-12-01 22:23:51 -0600

    Release pending @cjs-error/utils

--- index.js
+++ index.js
@@ -1,71 +1,113 @@
-var Error = module.exports = require('@cjs-error/extend');
+var Error  = require('@cjs-error/extend');
+// var assign = require('@cjs-error/utils').assign;
+
+module.exports = Error;
 
 function type(name, prototype) {
-  // i.e. `Constructor.type()` in which case `Super === Constructor`
-  var Super = this, Constructor;
+  // i.e. `constructor.type()` in which case `Super === constructor`
+  var Super = this, constructor, argType = typeof(name);
 
-  if (typeof(name) === 'object') {
+  if (argType === 'object') {
     // If `name` is typeof "object" then assume name is prototype
-    if (type[name.name]) {
-      Constructor = type[name.name];
-      // If cached then add all enumerable properties by descriptor to `Constructor.prototype`
-      if (Constructor) {
-        return assign(Constructor.prototype, name);
-      }
+    prototype = name;
+    name = prototype.name;
+    if (type[name]) {
+      constructor = type[name];
+      // If cached then add all enumerable properties by descriptor to `constructor.prototype`
+      return assign(constructor.prototype, prototype);
     }
 
     // Create a new constructor and return it, same as calling `Super.extend` directly without caching
-    Constructor = Super.extend.call(Super, name);
-    return type[Constructor.name] = Constructor;
+    constructor = Super.extend(prototype);
+    return type[name] = constructor;
+  }
+  else if (argType === 'function') {
+    // Extend and cache the constructor, Notice: that it doesn't matter if type[name] is already cached
+    constructor = Super.extend.apply(Super, arguments);
+    return type[name.name] = constructor;
   }
 
-  // `typeof` name is known only to not be an "object", if cached return that type
+  // Notice: `typeof` name is known only to not be an "object" and "function", if cached return that type
   if (type[name]) {
     return type[name];
   }
 
-  // Create a new constructor and return it, same as calling `Super.extend` directly without caching
+  // Extend and cache the constructor
   return type[name] = Super.extend.apply(Super, arguments);
 }
 
-Object.defineProperty(Error, 'type', { configurable: true, writable: true, value: type });
+Object.defineProperties(Error, {
+  type: {
+    configurable: true,
+    writable: true,
+    value: type
+  },
+  exists: {
+    configurable: true,
+    writable: true,
+    value: Function.call.bind(Object.prototype.hasOwnProperty, type)
+  }
+});
 
-
-// types are defined below
-// -----------------------------------------------------------------------------------------------------
-
-Object.defineProperty(Error.type, 'ErrorEmitter', {
-  configurable: true,
-  get() {
-    if (delete this.ErrorEmitter) {
-      var EventEmitter = require('events');
-
-      function ErrorEmitter(message, constructor) {
-        constructor = constructor || ErrorEmitter;
-
-        var error = Error.call(this, message, constructor);
-
-        constructor.emit('*', error);
-        constructor.emit(error.name, error);
-
-        return error;
+// Lazy Error types
+Object.defineProperties(Error.type, {
+  SystemError: {
+    configurable: true,
+    get() {
+      if (delete this.SystemError) {
+        return this.SystemError = Error.extend({
+          name: 'SystemError',
+          with: function (code, errno, path) {
+            this.code = code;
+            this.errno = errno;
+            this.path = path;
+            return this;
+          }
+        });
       }
+    },
+    set(constructor) {
+      if (delete this.SystemError) {
+        this.SystemError = constructor;
+      }
+    }
+  },
+  ErrorEmitter: {
+    configurable: true,
+    get() {
+      if (delete this.ErrorEmitter) {
+        var EventEmitter = require('events');
 
-      Object.assign(ErrorEmitter, EventEmitter.prototype);
-      EventEmitter.call(ErrorEmitter);
+        function ErrorEmitter(message, constructor) {
+          constructor = constructor || ErrorEmitter;
 
-      var _extend = Error.extend;
-      ErrorEmitter.extend = function extend(name, prototype) {
-        function ExtendedError(message, constructor) {
-          return ErrorEmitter.call(this, message, constructor || ExtendedError);
+          var error = Error.call(this, message, constructor);
+
+          constructor.emit('*', error);
+          constructor.emit(error.name, error);
+
+          return error;
         }
 
-        _extend.call(ErrorEmitter, ExtendedError, name, prototype);
-        EventEmitter.call(ExtendedError);
-        return ExtendedError;
-      };
+        Object.assign(ErrorEmitter, EventEmitter.prototype);
+        EventEmitter.call(ErrorEmitter);
 
-      return Error.type.ErrorEmitter = Error.extend(ErrorEmitter, 'ErrorEmitter');
+        var _extend = Error.extend;
+        ErrorEmitter.extend = function extend(name, prototype) {
+          function ExtendedError(message, constructor) {
+            return ErrorEmitter.call(this, message, constructor || ExtendedError);
+          }
+
+          return _extend.call(ErrorEmitter, ExtendedError, name, prototype);
+        };
+
+        return Error.type.ErrorEmitter = Error.extend(ErrorEmitter, 'ErrorEmitter');
+      }
+    },
+    set(constructor) {
+      if (delete this.ErrorEmitter) {
+        this.ErrorEmitter = constructor;
+      }
     }
   }
 });
